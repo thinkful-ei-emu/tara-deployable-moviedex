1b75801e2adfb27f99e1445b13afac4f404bc931
Tara Patel <t.jeanne.patel@gmail.com>
2019-07-19 14:36:34 -0500

    updated to be deployed to heroku

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-const express = require("express");
-const morgan = require("morgan");
+const express = require('express');
+const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
-const array = require("./movies-data-small");
-require("dotenv").config();
+const array = require('./movies-data-small');
+require('dotenv').config();
 
 const server = express();
 
-server.use(morgan("dev"));
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common';
+server.use(morgan(morganSetting));
 server.use(helmet());
 server.use(cors());
 
 server.use(function validateBearerToken(req, res, next) {
     const authToken = req.get('Authorization');
     const apiToken = process.env.API_TOKEN;
-    console.log('validate bearer token');
 
     if (!authToken || authToken.split(' ')[1] !== apiToken) {
         return res.status(401).json({error: 'Unauthorized request'});
@@ -32,9 +32,9 @@ function handleGetMovies(req, res) {
       return movie.genre.toLowerCase().includes(genre.toLowerCase());
     });
 
-    // if(movies === []){
-    //     return res.status(404).json("Please enter a valid genre")
-    // }
+    if(!movies.length){
+        return res.status(404).json('Please enter a valid genre');
+    }
   }
 
   if (country) {
@@ -42,15 +42,15 @@ function handleGetMovies(req, res) {
       return movie.country.toLowerCase().includes(country.toLowerCase());
     });
 
-    //  if(!movies){
-    //     return res.status(404).json("Please enter a valid country")
-    // }
+     if(!movies.length){
+        return res.status(404).json("Please enter a valid country");
+    }
   }
 
   if (avg_vote) {
-    // if(Number(avg_vote) === NaN){
-    //     return res.status(404).json("Enter a valid number")
-    // }
+    if(!parseInt(avg_vote)){
+        return res.status(404).json("Enter a valid number");
+    }
 
     movies = movies.filter(movie => {
       return Number(movie.avg_vote) >= Number(avg_vote);
@@ -58,15 +58,25 @@ function handleGetMovies(req, res) {
   }
 
   if (movies === []) {
-    return res.status(404).json("Please enter a valid country");
+    return res.status(404).json('Please enter a valid country');
   }
 
   res.status(200).json(movies);
 }
 
-server.get("/movie", handleGetMovies);
+server.get('/movie', handleGetMovies);
 
-const PORT = 8000;
+server.use((error, req, res, next) => {
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }};
+  } else {
+    response = { error };
+  }
+  res.status(500).json(response);
+});
+
+const PORT = process.env.PORT || 8000;
 
 server.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
